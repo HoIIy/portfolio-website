d1ac0b1124bdc786e4d56351b05bc1eb22ebd0e7
HoIIY <hmb1407@g.rit.edu>
2018-02-06 14:13:22 -0500

    Site structure and parallax placeholder.

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hillary Borrino</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- Optional Bootstrap theme -->
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+	<style type="text/css">
+		* {
+		margin: 0;
+			padding: 0;
+		}
+		html, body {
+			height: 100%;
+		}
+		
+		#pg_container {
+			height: 100%;
+		}
+		
+		#pg_first {
+			background-image: url("img/bg.jpg");
+			background-position: center;
+			background-repeat: no-repeat;
+			background-size: cover;
+			-webkit-filter: blur(0.2em);
+			filter: blur(0.2em);
+		}
+		
+		#pg_2 {
+			background-color: #fff;
+		}
+		
+		#pg_last {
+			background-color: #1e1e1e;
+		}
+	</style>
+</head>
+<body>
+<!--<div id="pg_container" class="container-fluid">
+    <div class="row">
+        <div id="page_1" class="col-12 h-100" style="background-color: #cdcdcd;">
+			left (starry)
+		</div>
+		
+		<div id="page_2" class="col-12 h-100" style="background-color: #ababab;">
+			p2
+		</div>
+    </div>
+</div>-->
+
+<div id="pg_container" class="container-fluid h-100">
+	<div class="row h-100">
+		<div id="pg_first" class="page col-12">Height 100%</div>
+	</div>
+	
+	<div class="row h-100">
+		<div id="pg_2" class="page col-12">Height 100%</div>
+	</div>
+</div>
+
+    
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
