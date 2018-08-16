66b09dd21ba4955d3609438db6b6cfedc7b29f3e
Holly Borrino <hmb1407@g.rit.edu>
2018-08-16 17:59:23 -0400

    Adds hover effect to logo home link + social media bar

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="assets/index.css">
 	<link rel="stylesheet" href="assets/material_design.css">
+	<link rel="stylesheet" href="lib/hover-min.css">
 
     <title>HB Portfolio</title>
 	</head>
@@ -19,8 +20,14 @@
   <header>
     <div class="z-depth-3">  
 		<div id="logo_bar" class="light_theme">
-		  <div id="logo">HB</div>
-		  <div id="bar_buttons">[sun] [moon] [rainbow]</div>
+			<div id="logo" class="hvr-float-shadow"><a href="/p6">HB</a></div>
+			<div id="bar_buttons">[sun] [moon] [rainbow]</div>
+		</div>
+		<div id="soc_med">
+			<img src="assets/sm_icons/mail.png"></img>
+			<img src="assets/sm_icons/twitter.png"></img>
+			<img src="assets/sm_icons/facebook.png"></img>
+			<img src="assets/sm_icons/linkedin.png"></img>
 		</div>
 	</div>
     
@@ -29,7 +36,7 @@
 	</div>
 	
     <nav class="light_theme z-depth-3">
-	  <ul><li class="first"><a href="#">Skills</a></li
+	  <ul><li class="first"><a href="#">Me</a></li
 		><li><a href="#">Portfolio</a></li
 		><li><a href="#">Resume</a></li
 		><li><a href="#">DevBlog</a></li
